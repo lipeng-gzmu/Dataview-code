a819cbd9aa99ec5d989b730df0c8b5c74fff9dab
lipeng-gzmu <1826427954@qq.com>
2021-05-19 16:00:54 +0800

    李鹏的第五次提交

--- js/bing.js
+++ js/bing.js
@@ -17,7 +17,7 @@ var placeHolderStyle = {
 };
 option = {
     title: {
-        text: '你幸福吗？',
+        text: '国民经济的恢复',
         //subtext: 'From ExcelHome',
         //sublink: 'http://e.weibo.com/1341556070/AhQXtjbqh',
         x: 'center',
@@ -26,7 +26,7 @@ option = {
         textStyle : {
             color : 'rgba(30,144,255,0.8)',
             fontFamily : '微软雅黑',
-            fontSize : 35,
+            fontSize : 10,
             fontWeight : 'bolder'
         }
     },
@@ -37,67 +37,50 @@ option = {
     legend: {
         orient : 'vertical',
         //x : document.getElementById('main').offsetWidth / 2,
-        x:'right',
-        y : 45,
+        x:'80px',
+        y : 55,
         itemGap:12,
-        data:['68%的人表示过的不错','29%的人表示生活压力很大','3%的人表示“我姓曾”']
+        data:['其它','重工业']
     },
    
     series : [
         {
-            name:'1',
+            name:'类型',
             type:'pie',
             clockWise:false,
-            radius : [85, 110],
+            radius : [85, 130],
             itemStyle : dataStyle,
             data:[
                 {
-                    value:68,
-                    name:'68%的人表示过的不错'
+                    value:91,
+                    name:'其它'
                 },
                 {
-                    value:32,
+                    value:9,
                     name:'invisible',
                     itemStyle : placeHolderStyle
                 }
             ]
         },
         {
-            name:'2',
+            name:'类型',
             type:'pie',
             clockWise:false,
-            radius : [60, 85],
+            radius : [40, 85],
             itemStyle : dataStyle,
             data:[
                 {
-                    value:29, 
-                    name:'29%的人表示生活压力很大'
+                    value:9, 
+                    name:'重工业'
                 },
                 {
-                    value:71,
+                    value:91,
                     name:'invisible',
                     itemStyle : placeHolderStyle
                 }
             ]
         },
-        {
-            name:'3',
-            type:'pie',
-            clockWise:false,
-            radius : [35, 60],
-            itemStyle : dataStyle,
-            data:[
-                {
-                    value:3, 
-                    name:'3%的人表示“我姓曾”'
-                },
-                {
-                    value:97,
-                    name:'invisible',
-                    itemStyle : placeHolderStyle
-                }
-            ]
-        }
+      
     ]
 };
 
